dba8d92fd37d644414ea81a65e9c615930d0df42
Mit Kotak <mitkotak0305@gmail.com>
2024-06-25 22:00:51 -0400

    consolidated the tests as well

--- tests/experimental/test_fulltp.py
+++ tests/experimental/test_fulltp.py
@@ -3,29 +3,27 @@ from e3nn import o3, o2, experimental
 import pytest
 
 
-@pytest.mark.parametrize("irreps_in1", ["0e", "0e + 1e"])
-@pytest.mark.parametrize("irreps_in2", ["2x0e", "2x0e + 3x1e"])
-def test_fulltp_o3(irreps_in1, irreps_in2):
-    irreps_in1 = o3.Irreps(irreps_in1)
+@pytest.mark.parametrize(
+    "group, irreps_in1, irreps_in2",
+    [
+        (o3, "0e", "2x0e"),
+        (o3, "0e", "2x0e + 3x1e"),
+        (o3, "0e + 1e", "2x0e"),
+        (o3, "0e + 1e", "2x0e + 3x1e"),
+        (o2, "0e", "2x0e"),
+        (o2, "0e", "2x0e + 3x1"),
+        (o2, "0e + 1", "2x0e"),
+        (o2, "0e + 1", "2x0e + 3x1"),
+    ],
+)
+def test_fulltp(group, irreps_in1, irreps_in2):
+    irreps_in1 = group.Irreps(irreps_in1)
+    irreps_in2 = group.Irreps(irreps_in2)
+
     x = irreps_in1.randn(10, -1)
-    irreps_in2 = o3.Irreps(irreps_in2)
     y = irreps_in2.randn(10, -1)
 
     tp_pt2 = torch.compile(experimental.FullTensorProductv2(irreps_in1, irreps_in2), fullgraph=True)
-    tp = o3.FullTensorProduct(irreps_in1, irreps_in2)
-
-    torch.testing.assert_close(tp_pt2(x, y), tp(x, y))
-
-
-@pytest.mark.parametrize("irreps_in1", ["0e", "0e + 1"])
-@pytest.mark.parametrize("irreps_in2", ["2x0e", "2x0e + 3x1"])
-def test_fulltp_o2(irreps_in1, irreps_in2):
-    irreps_in1 = o2.Irreps(irreps_in1)
-    x = irreps_in1.randn(10, -1)
-    irreps_in2 = o2.Irreps(irreps_in2)
-    y = irreps_in2.randn(10, -1)
-
-    tp_pt2 = torch.compile(experimental.FullTensorProductv2(irreps_in1, irreps_in2), fullgraph=True)
-    tp = o2.FullTensorProduct(irreps_in1, irreps_in2)
+    tp = group.FullTensorProduct(irreps_in1, irreps_in2)
 
     torch.testing.assert_close(tp_pt2(x, y), tp(x, y))
